78c9374226dc866b5027d1f5754772c0d08fb47f
nine <>
2021-09-21 12:02:49 +0900

    initial commit

---
+++ Color/Color/SceneDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  SceneDelegate.swift
+//  Color
+//
+//  Created by Byoung-Hwi Yoon on 2021/09/21.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        window = UIWindow(windowScene: windowScene)
+        
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+    }
+}
+
---
+++ Color/Color/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Color
+//
+//  Created by Byoung-Hwi Yoon on 2021/09/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+
+
+}
+
